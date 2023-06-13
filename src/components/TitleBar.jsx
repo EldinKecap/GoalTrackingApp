f5b1cf4131a45efa871f3376652fcf56404cac50
EldinKecap <76916396+EldinKecap@users.noreply.github.com>
2023-06-13 15:28:00 +0200

    rewrote title bar

--- src/components/TitleBar.jsx
+++ src/components/TitleBar.jsx
@@ -1,31 +1,8 @@
 import { AppBar, Typography } from "@mui/material";
 import React from "react";
 
-export default function TitleBar() {
-    let title;
-
-    switch (location.pathname) {
-        case "/":
-            title = "Goal list"
-            break;
-        case "/create":
-            title = "Create a goal"
-            break;
-        case "/edit":
-            title = "Edit a goal"
-            break;
-        case "/login":
-            title = "Login"
-            break;
-        case "/createAccount":
-            title = "Create an account"
-            break;
-        default:
-            title = "Error message";
-            break;
-    }
-
-
+export default function TitleBar({title}) {
+    
   return (
     <AppBar elevation={0}>
       <Typography variant="h4" component="h1" m={0.5}>
